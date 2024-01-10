776befc0585b4f0556fd9c6ecbbe39e882552b0e
Potatowo233 <2224271513@qq.com>
2024-01-11 05:24:55 +0800

    Site updated: 2024-01-11 05:24:53

--- js/main.js
+++ js/main.js
@@ -136,4 +136,6 @@
         $mask.on('click', toggleToc);
         $('.navbar-main .catalogue').on('click', toggleToc);
     }
+    $('div.container div.card[data-type=tags]').addClass('is-hidden-mobile');
+    $('div.container div.card[data-type=archives]').addClass('is-hidden-mobile');
 }(jQuery, window.moment, window.ClipboardJS, window.IcarusThemeSettings));
